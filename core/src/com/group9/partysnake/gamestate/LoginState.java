307f6058353b7c7ec52f62af1d8b2cdf8751bb68
David Bendiksen <davidbendiksen@hotmail.com>
2021-04-28 16:58:58 +0200

    Pre-rebase commit

--- core/src/com/group9/partysnake/gamestate/LoginState.java
+++ core/src/com/group9/partysnake/gamestate/LoginState.java
@@ -1,6 +1,7 @@
 package com.group9.partysnake.gamestate;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -8,7 +9,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,14 +16,12 @@ import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
-public class LoginState extends GameState {
+public class LoginState extends State {
     private GameStateManager gsm;
 
     private Stage stage;
     private TextField usernameField, passwordField;
     private TextButton loginButton, newUserButton;
-
-    private Socket socket;
 
     private String HOSTNAME = "127.0.0.1";
     private int PORT = 3000;
@@ -31,26 +29,26 @@ public class LoginState extends GameState {
     LoginState(GameStateManager gsm) {
         super(gsm);
 
-        this.stage = new Stage();
+        stage = new Stage();
         TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
-        this.usernameField = new TextField("Username", textFieldStyle);
-        this.passwordField = new TextField("Password", textFieldStyle);
+        usernameField = new TextField("Username", textFieldStyle);
+        passwordField = new TextField("Password", textFieldStyle);
         TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
-        this.loginButton = new TextButton("Log in", buttonStyle);
-        this.newUserButton = new TextButton("Create user", buttonStyle);
+        loginButton = new TextButton("Log in", buttonStyle);
+        newUserButton = new TextButton("Create user", buttonStyle);
 
         Gdx.input.setInputProcessor(stage);
-        this.stage.addActor(usernameField);
-        this.stage.addActor(passwordField);
-        this.stage.addActor(loginButton);
-        this.stage.addActor(newUserButton);
-        this.loginButton.addListener(new ChangeListener() {
+        stage.addActor(usernameField);
+        stage.addActor(passwordField);
+        stage.addActor(loginButton);
+        stage.addActor(newUserButton);
+        loginButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 connect(false);
             }
         });
-        this.newUserButton.addListener(new ChangeListener() {
+        newUserButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 connect(true);
@@ -58,18 +56,23 @@ public class LoginState extends GameState {
         });
     }
 
+    @Override
+    public void handleInput() {
+
+    }
+
     private void connect(boolean newUser) {
         URI uri = URI.create( String.format("http://%s:%d/", this.HOSTNAME, this.PORT) );
 
-        Map<String, String> auth = new HashMap<String, String>();
+        Map<String, String> auth = new HashMap<>();
         auth.put("name", this.usernameField.getText());
         auth.put("password", this.passwordField.getText());
         auth.put("new", Boolean.toString(newUser));
 
         IO.Options options = IO.Options.builder().setAuth(auth).build();
 
-        this.socket = new IO.socket(uri, options);
-        this.socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+        gsm.socket = new IO.socket(uri, options);
+        gsm.socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
             @Override
             public void call(Object... args) {
                 System.out.println(String.format("Could not connect.\nError: %s\nReason: %s",
@@ -87,6 +90,11 @@ public class LoginState extends GameState {
     }
 
     public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
 
     }
 
