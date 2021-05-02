2b3a60cd03d333633025b600a0b5d3b74d2c22ff
TheMangelo <mikkelangeloj@gmail.com>
2021-05-02 16:36:47 +0200

    Made a way to read from JSONarray to data for the ScoreState as well as a testClass

--- core/src/com/group9/partysnake/gamestate/ScoreState.java
+++ core/src/com/group9/partysnake/gamestate/ScoreState.java
@@ -2,6 +2,7 @@ package com.group9.partysnake.gamestate;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -12,21 +13,43 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.group9.partysnake.PartySnake;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ScoreState extends State{
     private Stage stage;
     private Button backButton;
-    private Texture background, highscore, name, score;
-    private String names[];
+    private Texture backgroundTexture, highscoreTexture, nameTexture, scoreTexture;
+    private boolean loaded = false;
+    private BitmapFont font;
 
+
+    // DATA: [{“player”: DavidF, “Score”: 4000} , {“player”: Mikkel, “Score”: 23}
+    private ArrayList<HashMap<String,String>> scoreResult;
+    private ArrayList scoreList;
 
     public ScoreState(GameStateManager gsm) {
         super(gsm);
 
         stage = new Stage();
-        background = new Texture("background.png");
-        highscore = new Texture("highscore.png");
-        name = new Texture ("name.png");
-        score = new Texture ("score.png");
+        backgroundTexture = new Texture("background.png");
+        highscoreTexture = new Texture("highscore.png");
+        nameTexture = new Texture ("name.png");
+        scoreTexture = new Texture ("score.png");
+        font = new BitmapFont();
+        font.getData().setScale(2f,2f);
+
+        jsonFromServer testJson = new jsonFromServer();
+
+        try{
+            convertJson(testJson.getTestJsonScore());
+        }  catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         makeButton();
         clickHandling();
@@ -39,9 +62,7 @@ public class ScoreState extends State{
     private void makeButton() {
         TextureRegion backArrow = new TextureRegion(new Texture("return_arrow.png"));
         TextureRegionDrawable backDrawable = new TextureRegionDrawable(backArrow);
-
         backButton = new Button(backDrawable);
-
         backButton.setTransform(true);
         backButton.setScale(0.5f);
         backButton.setPosition(20, 310);
@@ -56,10 +77,8 @@ public class ScoreState extends State{
         });
     }
 
-
     @Override
     public void handleInput() {
-
     }
 
     @Override
@@ -70,19 +89,65 @@ public class ScoreState extends State{
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(background, 0, 0);
-        sb.draw(highscore, 130, 280, 400, 70);
-        sb.draw(name, 240, 230, 70, 20);
-        sb.draw(score, 470, 230, 70, 20);
+        sb.draw(backgroundTexture, 0, 0);
+        sb.draw(highscoreTexture, 130, 280, 400, 70);
+        sb.draw(nameTexture, 240, 250, 70, 20);
+        sb.draw(scoreTexture, 470, 250, 70, 20);
+
+        if(!loaded){
+            //font.draw(sb,"Loading...", 240,30);
+
+
+
+        }
+        else if (loaded){
+            int startYpos = 230;
+
+            for(int i = 0; i < scoreResult.size(); i++){
+
+                HashMap<String,String> tempHash = scoreResult.get(i);
+                String tempPlayer = tempHash.get("player");
+                String tempScore = tempHash.get("score");
+                font.draw(sb,i+1+"", 200,startYpos);
+                font.draw(sb,tempPlayer, 250,startYpos);
+                font.draw(sb,tempScore, 470,startYpos);
+                startYpos -= 40;
+            }
+
+        }
+
+
+
+
         sb.end();
         stage.draw();
     }
 
     @Override
     public void dispose() {
-        background.dispose();
-        highscore.dispose();
-        name.dispose();
-        score.dispose();
+        backgroundTexture.dispose();
+        highscoreTexture.dispose();
+        nameTexture.dispose();
+        scoreTexture.dispose();
+    }
+
+    public void convertJson(JSONArray fromServer) throws JSONException {
+        ArrayList scores = new ArrayList();
+
+
+        try {
+            for (int i = 0; i < fromServer.length(); i++) {
+                JSONObject tempObj = fromServer.getJSONObject(i);
+                HashMap<String, String> tempHash = new HashMap<String, String>();
+                tempHash.put("player", (String) tempObj.get("player"));
+                tempHash.put("score", (String) tempObj.get("score"));
+                scores.add(tempHash);
+            }
+            scoreResult = scores;
+            loaded = true;
+        }
+        catch(JSONException e){
+            System.out.println("Error getting JSONs");
+        }
     }
 }
