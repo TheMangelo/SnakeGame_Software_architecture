c93f57b3883056031e43e398bf62fcdb94c30089
David Bendiksen <davidbendiksen@hotmail.com>
2021-05-03 22:15:01 +0200

    Formatting

--- core/src/com/group9/partysnake/gamestate/MenuState.java
+++ core/src/com/group9/partysnake/gamestate/MenuState.java
@@ -1,38 +1,13 @@
 package com.group9.partysnake.gamestate;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Rectangle;
-import com.group9.partysnake.GameScreen;
 import com.group9.partysnake.PartySnake;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.scenes.scene2d.ui.Stack;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MenuState extends State {
     private Texture title, setting, online, single, score;
@@ -78,8 +53,6 @@ public class MenuState extends State {
     @Override
     public void handleInput() {
         if (Gdx.input.justTouched()) {
-            System.out.println("You just touched the screen");
-
             int x = Gdx.input.getX();
             int y = PartySnake.HEIGHT - Gdx.input.getY();
 
@@ -89,8 +62,8 @@ public class MenuState extends State {
             } else if ( (width - online.getWidth() < x && x < width) &&
                     (height - GRID_CELL*8 < y && y < height-GRID_CELL*8 + online.getHeight()) ) {
                 gsm.push(new LoginState(gsm, true));
-            } else if ((0 < x && x > score.getWidth()) &&
-                    (GRID_CELL*2 < y && y < GRID_CELL*2 + score.getHeight())) {
+            } else if ( (0 < x && x > score.getWidth()) &&
+                    (GRID_CELL*2 < y && y < GRID_CELL*2 + score.getHeight()) ) {
                 gsm.push(new LoginState(gsm, false));
             }
         }
@@ -116,7 +89,7 @@ public class MenuState extends State {
     public void render(SpriteBatch sb) {
         clearScreen();
         draw(sb);
-        //drawGrid();
+        // drawGrid();
     }
 
     public void dispose() {
